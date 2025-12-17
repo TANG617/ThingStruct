5393a01382ac372813723db131d1baf1557c25fc
LiTang <litang0617@outlook.com>
2025-12-17 11:08:34 +0800

    Initial Commit

---
+++ ThingStruct/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ThingStruct
+//
+//  Created by TimLi on 2025/12/17.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
